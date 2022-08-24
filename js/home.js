adfc083b485404e03cfb80fa9283143fd28c660d
ellieeadington <ellieeadington5@gmail.com>
2022-08-24 12:49:05 +0100

    made the function work

--- js/home.js
+++ js/home.js
@@ -5,7 +5,17 @@
 const scoreArr = [];
 const dLIndexArr = [];
 let dL = localStorage.getItem("dateTime");
+console.log(typeof dL);
+
 console.log(dL);
+dL = dL.replaceAll('","',"'");
+dL = dL.replaceAll('["',"'");
+dL = dL.replaceAll('"]',"'");
+
+console.log(dL);
+
+//'24/08/2022, 10:45:44'24/08/2022, 11:44:42'24/08/2022, 11:45:15'24/08/2022, 11:52:37'
+
 
 //-----------------------------------------------------------------------------------------------------------------------
 function updateScoreBoard() {
@@ -16,26 +26,29 @@ function nth_ocurrence() {
 
   for (let i= 0; i <dL.length; i++) {
 
-    if (dL.charAt(i) == '"') {
+    if (dL.charAt(i) == "'") {
       dLIndexArr.push(i);   
-    }
+    }  
   }
 }
 nth_ocurrence();
-// console.log(dLIndexArr);
+console.log(dLIndexArr);
 
 function populateDL() {
+
   for (let i = 0; i < dLIndexArr.length; i++) {
-    let firstIndex = dLIndexArr[i];
-    let secondIndex = dLIndexArr[i+1];
-    // console.log(firstIndex);
-    // console.log(secondIndex);
-    let datei = dL.substr(firstIndex+1,secondIndex-2);
+
+    let index = dLIndexArr[i];
+    console.log(index)
+    let datei = dL.substr(index+1,20);
+    console.log(datei);
     dLFinal.push(datei);
   }
 }
+
+
 populateDL();
-// console.log(dLFinal);
+console.log(dLFinal);
 
 dLFinal = dLFinal.filter(item => item != '');
 console.log(dLFinal);
@@ -96,4 +109,5 @@ console.log(dLFinal);
 }
 
 updateScoreBoard();
+
 
